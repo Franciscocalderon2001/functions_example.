bd21919c4d523abb40d7e4c132bc5dfd47b68737
Francisco <Franciscocalderon1@mail.weber.edu>
2022-11-22 18:03:40 -0700

    Parser function

--- functions.py
+++ functions.py
@@ -1,10 +1,52 @@
 # Conversion Calculator
 # By: Francisco Calderon
 
-# User input regarding the length to convert
-# Get the unit from the user
-# Convert the length to the correct unit
-# Output the answer to the user
+# user input regarding the length to convert
+# get the unit from the user
+# convert the length to the correct unit
+# output the answer to the user
+def user_parser(user_input):
 
-user_number = input("What number to convert? ")
-print (user_number)
+values = user_input.rsplit(" ")
+print (values)
+
+number = values[0]
+if number.isdigit():
+    number = float(number)
+else:
+    print("That is not a valid number")
+user_input = input("number and unit to convert ")
+user_parser(user_input)
+
+
+
+return number, 
+
+while True:
+    user_input = input("number and unit to convert ")
+    user_number, user_unit = user_parser(user_input)
+    print('User number', user_number)
+    print('User unit', user_unit)
+# while True:
+# user_number = input("What number to convert? ")
+# if user_number.isdigit():
+# user_number = float(user_number)
+# break
+# else:
+# print ('please use a number')
+# user_unit = input("What unit is your number?")
+
+# if(user_unit == 'in'):
+# #perform in to mm
+# conv_number = user_number * 25.4
+# conv_unit = 'mm'
+# break
+# elif(user_unit == 'mm'):
+# #perform mm to in
+# conv_number = user_number / 25.4
+# conv_unit = 'in'
+# break
+# else:
+# print('That is not a valid unit')
+
+# print(conv_number, conv_unit)
